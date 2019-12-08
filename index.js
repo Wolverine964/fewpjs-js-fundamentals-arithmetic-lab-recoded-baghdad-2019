2f0197cc55f58960ea2430db33a14674e6e367f6
ibraheem Hasan <rules656@gmail.com>
2019-12-08 17:14:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let n1 =31;
-let n2 = 2;
-let multiply = n1 * n2;
+let num1 =31;
+let num2 = 2;
+let multiply = num1 * num2;
 let max = Math.max(1,2,3,20)
 let random = Math.floor(Math.random() * 10) + 1;
 let mod = 24 % 5;
